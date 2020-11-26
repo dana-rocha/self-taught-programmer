dc713f6a746cd8a75e93952b62e2253c49e135f5
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2020-11-25 23:01:52 -0800

    Chapter 5 Examples Completed

--- chapter-5/ch5-dictionary-examples.py
+++ chapter-5/ch5-dictionary-examples.py
@@ -46,6 +46,93 @@ facts["founded"]
 # Can use the "in" keyword
 bill = {"Bill Gates": "charitable"}
 
+# Check if key is in dictionary
 "Bill Gates" in bill
 
+# Check if key is not in dictionary
+"Bill Doors" not in bill
 
+
+# Deleting key-value pairs
+
+books = {"Dracula": "Stoker", "1984": "Orwell", "The Trial": "Kafka"}
+
+del books["The Trial"]
+
+books
+
+# Example using a dictionary
+rhymes = {"1": "fun", "2": "blue", "3": "me", "4": "floor", "5": "live"}
+
+n = input("Type a number:")
+
+if n in rhymes:
+    rhyme = rhymes[n]
+    print(rhyme)
+else:
+    print("Not found.")
+
+
+
+# Nested Containers 1
+lists = []
+
+rap = ["Kanye West", "Jay Z", "Eminem", "Nas"]
+
+rock = ["Bob Dylan", "The Beatles", "Led Zepplin"]
+
+djs = ["Zeds Dead", "Tiesto"]
+
+lists.append(rap)
+lists.append(rock)
+lists.append(djs)
+
+print(lists)
+
+
+rap = lists[0]
+print(rap)
+
+rap.append("Kendrick Lamar")
+
+print(rap)
+print(lists)
+
+# You can store a tuple inside a list, a list inside a tuple, and a dictionary inside of a list or tuple
+
+# Nested Containers 2
+locations = []
+
+la = (34.0522, 188.2437)
+chicago = (41.8771, 87.6298)
+
+locations.append(la)
+locations.append(chicago)
+
+# A list of tuples
+print(locations)
+
+
+# Nested Containers 3
+eights = ["Edgar Allan Poe", "Charles Dickens"]
+
+nines = ["Hemingway", "Fitzgerald", "Orwell"]
+
+# A tuple of lists
+authors = (eights, nines)
+print(authors)
+
+
+# Nested Containers 4
+bday = {"Hemingway": "7.21.1899", "Fitzgerald": '9.24.1896'}
+
+# A tuple of dictionaries
+my_list = [bday]
+print(my_list)
+
+my_tuple = [bday]
+print(my_tuple)
+
+
+# A list, tuple, or dictionary can be a value in a dictionary
+ny = {"location": (40.7128, 74.0059), "celebs": ["W. Allen", "Jay Z", "K. Bacon"], "facts": {"state": "NY", "country": "America"}}
