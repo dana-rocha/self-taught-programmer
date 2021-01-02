1513eafdab664db5308c2505e60652aa6fbbff35
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2021-01-01 23:46:31 -0800

    Updates

---
+++ chapter-10/list_comprehensions.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Exercise 7 from practicepython.org
+# Dana Rocha 1/1/21
+
+# Given a list, write one line of Python that takes this list and makes a new list that has only the even elements in it
+
