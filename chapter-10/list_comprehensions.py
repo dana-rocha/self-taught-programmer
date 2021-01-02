20b4c922bf4c74c52affb3c08f9ea47432ec0cf9
Dana Rocha <46910672+dana-rocha@users.noreply.github.com>
2021-01-01 23:57:13 -0800

    Python Practice Exercise 7

--- chapter-10/list_comprehensions.py
+++ chapter-10/list_comprehensions.py
@@ -5,3 +5,15 @@
 
 # Given a list, write one line of Python that takes this list and makes a new list that has only the even elements in it
 
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Long way
+for i in a:
+    if i % 2 == 0:
+        print(i)
+
+
+# Using list comprehension
+b = [x for x in a if x % 2 == 0]
+
+print(b)
